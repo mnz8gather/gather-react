8371cf7c6927ff53a91b4c22e2c7ec76d65945cf
mnz8 <mnz8@foxmail.com>
2022-09-26 00:28:15 +0800

    feat: sort by Intl.Collator

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -5,9 +5,11 @@ const { Content, Sider } = Layout;
 
 const UmiLayout: React.FC = () => {
   const { routes } = useAppData();
+  const collator = new Intl.Collator('en');
 
   const items = Object.entries(routes)
     .filter(([, value]) => value.id !== '@@/global-layout')
+    .sort(([x], [y]) => collator.compare(x, y))
     .map(([key, value]) => ({
       label: <Link to={value.path!}>{value.id}</Link>,
       key,
