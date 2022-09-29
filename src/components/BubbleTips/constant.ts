bbce8d0deba0d7227821fe7f44f23fbe69caaf1b
mnz8 <mnz8@foxmail.com>
2022-09-30 00:08:00 +0800

    feat: BubbleTips,ScatterMap,Sound,CustomColumnModal

---
+++ src/components/BubbleTips/constant.ts
@@ -0,0 +1,5 @@
+const levelsArray = ['L111', 'L222', 'L333', 'L444'];
+
+const typesArray = ['T111', 'T222', 'T333', 'T444'];
+
+export { levelsArray, typesArray };
---
+++ src/components/BubbleTips/index.tsx
@@ -0,0 +1,2 @@
+export { default } from './BubbleTips';
+export * from './constant';
