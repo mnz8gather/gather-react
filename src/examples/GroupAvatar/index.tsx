399b66c05d46562f3229b349b2cd8a746721c725
mnz8 <mnz8@foxmail.com>
2023-02-15 22:39:21 +0800

    feat: group avatar

---
+++ src/examples/GroupAvatar/index.tsx
@@ -0,0 +1,28 @@
+import { Avatar } from 'antd';
+import type { AvatarProps } from 'antd';
+import styles from './index.less';
+
+const GroupAvatar = (props: GroupAvatarProps) => {
+  const { src, avatarProps } = props;
+
+  const maxCount = 9;
+  const srcShow = Array.isArray(src) ? src.slice(0, maxCount) : [];
+
+  return (
+    <div className={styles['group-avatar']} style={props.style}>
+      {srcShow.map((src, index) => (
+        <div className={styles['single-avatar']} key={`group-avatar-${index}`}>
+          <Avatar src={src} style={{ width: '100%', height: '100%' }} {...avatarProps} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default GroupAvatar;
+
+interface GroupAvatarProps {
+  style?: React.CSSProperties;
+  src?: string[];
+  avatarProps?: AvatarProps;
+}
---
+++ src/pages/GroupAvatar.tsx
@@ -0,0 +1,7 @@
+import GroupAvatar from '@/examples/GroupAvatar';
+
+export default () => {
+  const url = 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg';
+
+  return <GroupAvatar style={{ width: '100px', height: '100px' }} avatarProps={{ shape: 'square' }} src={[url, url, url]} />;
+};
