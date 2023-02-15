0d94d7e4896b6072747a2a87a0f381b09388db20
mnz8 <mnz8@foxmail.com>
2023-02-15 23:00:30 +0800

    feat: chat scroll loading

---
+++ src/examples/ChatScrollLoading/index.tsx
@@ -0,0 +1,134 @@
+import { useRef, ReactNode, Key } from 'react';
+import { useEventListener, useInfiniteScroll } from 'ahooks';
+import { Avatar } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+import ChatBubble from '@/components/ChatBubble';
+import styles from './index.less';
+
+interface Result {
+  list: any[];
+  total: number;
+  current: number;
+  pageSize: number;
+}
+
+const resultData = Array.from({ length: 100 }, (_, index) => ({
+  id: index,
+  content: '1234',
+  account: index % 7 === 0 ? 'mei13' : '567',
+  type: index % 5 === 0 ? 'notice' : 'normal',
+  nickname: index % 7 === 0 ? '梅十三' : '五六七',
+}));
+
+export default (props: { room_id?: any; currentAccount: any }) => {
+  const { room_id, currentAccount } = props;
+
+  const scrollRef = useRef<HTMLDivElement>(null);
+
+  useEventListener(
+    'wheel',
+    (event) => {
+      event.preventDefault();
+      // 调整滚轮方向
+      scrollRef?.current?.scrollBy?.(0, -event.deltaY);
+    },
+    { target: scrollRef },
+  );
+
+  async function requestChatContent(current: number, pageSize: number): Promise<Result> {
+    // normal use room_id request
+    const total = resultData.length;
+    const list = resultData.slice(current * pageSize, current * pageSize + pageSize);
+
+    const r = { total, list, current, pageSize };
+
+    return r;
+  }
+
+  // const hasNew = false;
+
+  const { data } = useInfiniteScroll(
+    (d) => {
+      const current = d ? d?.current + 1 : 0;
+      return requestChatContent(current, 20);
+    },
+    {
+      target: scrollRef,
+      isNoMore: (d) => {
+        if (d === undefined) {
+          return true;
+        }
+        return d?.current * d?.pageSize > d?.total;
+      },
+      // reloadDeps: [hasNew],
+      // manual: true,
+    },
+  );
+
+  return (
+    <>
+      <div
+        style={{
+          height: `600px`,
+          overflow: 'auto',
+        }}
+        className={styles['message-list']}
+        ref={scrollRef}
+      >
+        <div className={styles['message-view']}>
+          {data?.list.map((ele: { account: string; type: string; id: Key; content: string; nickname: string }) => {
+            const isCurrent = ele?.account === currentAccount;
+            if (ele.type === 'normal') {
+              return (
+                <div key={ele?.id} className={styles['message-list-item']}>
+                  <ChatItem
+                    content={<span style={{ wordBreak: 'break-all' }}>{ele.content}</span>}
+                    placement={isCurrent ? 'left' : 'right'}
+                    nickname={isCurrent ? undefined : ele.nickname}
+                  />
+                </div>
+              );
+            }
+            return (
+              <div key={ele?.id} className={styles['message-list-item']}>
+                <Notice>{ele.content}</Notice>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+function ChatItem(props: { placement: Placement; nickname: any; content: ReactNode }) {
+  const flexDirection = props?.placement === 'left' ? 'row-reverse' : undefined;
+
+  return (
+    <div style={{ display: 'flex', flexDirection }}>
+      <Avatar style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} />
+      <div className={styles['chat-item-right']}>
+        {props?.nickname && <Nickname>{props?.nickname}</Nickname>}
+        <ChatBubble content={props?.content} placement={props?.placement} bubbleStyle={{ flexBasis: 'fit-content' }} />
+      </div>
+    </div>
+  );
+}
+
+function Notice(props: { children: string }) {
+  return (
+    <div className={styles['chat-notice']}>
+      <div>{props?.children}</div>
+    </div>
+  );
+}
+
+function Nickname(props: { children: string }) {
+  return (
+    <div className={styles['chat-nickname']}>
+      <div>{props?.children}</div>
+    </div>
+  );
+}
+
+type Placement = 'left' | 'right';
