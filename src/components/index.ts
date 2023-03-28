e5e6a813f2bf469414e0e0f26e651549516c63e5
mnz8 <mnz8gg@gmail.com>
2023-03-28 22:59:49 +0800

    feat: 调整

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,8 @@
 import ChatBubble from './ChatBubble';
 import CheckSortModal from './CheckSortModal';
+import DropdownSortButton from './DropdownSortButton';
+import DropdownSortButtonControlled from './DropdownSortButtonControlled';
 import ScatterMap from './ScatterMap';
 import Sound from './Sound';
 
-export { ChatBubble, CheckSortModal, ScatterMap, Sound };
+export { ChatBubble, CheckSortModal, DropdownSortButton, DropdownSortButtonControlled, ScatterMap, Sound };
--- src/pages/DropdownSortButton.tsx
+++ src/pages/DropdownSortButton.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import DropdownSortButton from '@/components/DropdownSortButton';
+import { DropdownSortButton } from '@/components';
 import type { EffectiveConditions } from '@/components/DropdownSortButton/interface';
 
 const sortFields = [
