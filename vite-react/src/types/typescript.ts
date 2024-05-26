9ee47124679eca86290f3721e58fe1a8f933de68
mnz8 <mnz8@foxmail.com>
2024-05-26 23:27:59 +0800

    feat: types

--- vite-react/src/types/typescript.ts
+++ vite-react/src/types/typescript.ts
@@ -6,3 +6,30 @@
 interface MechaProps {
   render: <P extends any[]>(...args: P) => React.ReactNode;
 }
+
+/** 函数声明 */
+type FunctionSample = (a: number) => number;
+
+/** 函数声明 */
+interface FunctionSample2 {
+  (a: number): number;
+}
+
+/** 函数声明 */
+type FunctionSample3 = {
+  (a: number): number;
+};
+
+/** 函数重载 */
+interface FunctionOverload1 {
+  (a: number): number;
+  (a: string): string;
+}
+
+/** 函数重载 */
+type FunctionOverload2 = {
+  (a: number): number;
+  (a: string): string;
+};
+
+/** 对象方法声明 */
