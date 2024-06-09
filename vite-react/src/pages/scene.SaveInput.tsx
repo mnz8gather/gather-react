b51414c6c0e79ea2abe734a3c2bff1a89e34a4d5
mnz8 <mnz8@foxmail.com>
2024-06-09 23:59:10 +0800

    feat: SaveInput

---
+++ vite-react/src/pages/scene.SaveInput.tsx
@@ -0,0 +1,10 @@
+import { SaveInput } from '@/scene/Antd.SaveInput';
+import GeneralContainer from '@/alpha/layout/GeneralContainer';
+
+export default () => {
+  return (
+    <GeneralContainer>
+      <SaveInput buttonChildren='保存' styles={{ input: { width: '200px' } }} />
+    </GeneralContainer>
+  );
+};
