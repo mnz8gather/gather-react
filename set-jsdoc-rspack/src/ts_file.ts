f7667a59700529b61a3cffd3cc5442ca10459c2e
mnz8 <mnz8@foxmail.com>
2024-11-15 00:08:15 +0800

    feat: jsdoc

---
+++ set-jsdoc-rspack/src/ts_file.ts
@@ -0,0 +1,6 @@
+export interface OverviewData {
+  /** 总数 */
+  total: string;
+  /** 比率 */
+  rate: number;
+}
---
+++ set-jsdoc-rspack/src/use_ts_file_type.js
@@ -0,0 +1,5 @@
+/**
+ * @typedef {import('./ts_file').OverviewData} OverviewData
+ * @type {OverviewData}
+ */
+const overviewData = { rate: 66.6, total: '100' };
