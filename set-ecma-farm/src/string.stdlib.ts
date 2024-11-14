899d40c5c3da832778d881bc94da8dc7a9fc4fdf
mnz8 <mnz8@foxmail.com>
2024-11-15 00:57:20 +0800

    chore: format

--- set-ecma-farm/src/string.stdlib.ts
+++ set-ecma-farm/src/string.stdlib.ts
@@ -1,11 +1,16 @@
 // localeCompare
 
 // 普通英文字母，两者结果相同
-let str1 = "hello";
-console.log(str1.toUpperCase());        // "HELLO"
-console.log(str1.toLocaleUpperCase());  // "HELLO"
+let str1 = 'hello';
+// "HELLO"
+console.log(str1.toUpperCase());
+// "HELLO"
+console.log(str1.toLocaleUpperCase());
 
 // 特殊字符的情况
-let str2 = "istanbul";  // 土耳其语
-console.log(str2.toUpperCase());                    // "ISTANBUL"
-console.log(str2.toLocaleUpperCase('tr-TR'));      // "İSTANBUL"  注意 i 的转换不同
+// 土耳其语
+let str2 = 'istanbul';
+// "ISTANBUL"
+console.log(str2.toUpperCase());
+// "İSTANBUL"  注意 i 的转换不同
+console.log(str2.toLocaleUpperCase('tr-TR'));
